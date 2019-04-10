211a47c0c97b6d800e8cf0fb9b01f6a3e780650c
Michael Zöhling <michael.zoehling@outlook.com>
2019-04-10 16:21:19 +0200

    Operatorüberladung

---
+++ ErweiterungsmethodenUndOperatorüberladung/ErweiterungsmethodenUndOperatorüberladung/Bruch.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErweiterungsmethodenUndOperatorüberladung
+{
+    class Bruch
+    {
+        public Bruch(int zähler, int nenner)
+        {
+            Zähler = zähler;
+            Nenner = nenner;
+        }
+
+        public int Zähler { get; set; }
+        public int Nenner { get; set; }
+
+        // Operatoren
+        
+        // Arithmetische Operatoren:
+        // +,-,*,/,%, ++, --
+        // -> +=, -= wird automatisch mitüberladen
+
+        // Bit Operatoren
+        // &,|,^,~,<<,>>
+        // &=, |= ... ebenfalls
+
+        // Vergleichsoperatoren
+        // ==, !=, <=, >=, <, >
+        // Achtung: Paarweise überladen: Wenn man == überladet, MUSS man auch != überladen
+
+        // Nicht Überladbar: Verknüpfungsoperatoren wie &&, || 
+
+        public static Bruch operator *(Bruch left, Bruch right)
+        {
+            return new Bruch(left.Zähler * right.Zähler, left.Nenner * right.Nenner);
+        }
+        public static Bruch operator /(Bruch left, Bruch right)
+        {
+            return new Bruch(left.Zähler * right.Nenner, left.Nenner * right.Zähler);
+        }
+    }
+}
--- ErweiterungsmethodenUndOperatorüberladung/ErweiterungsmethodenUndOperatorüberladung/Program.cs
+++ ErweiterungsmethodenUndOperatorüberladung/ErweiterungsmethodenUndOperatorüberladung/Program.cs
@@ -9,11 +9,19 @@ namespace ErweiterungsmethodenUndOperatorüberladung
     {
         static void Main(string[] args)
         {
-
             // Erweiterungsmethoden:
-            string text = "Hallo Welt !";
-            Console.WriteLine(text.Umdrehen());
-            Console.WriteLine(Erweiterungsmethoden.Umdrehen(text));
+            //string text = "Hallo Welt !";
+            //Console.WriteLine(text.Umdrehen());
+            //Console.WriteLine(Erweiterungsmethoden.Umdrehen(text));
+
+            // Operatorüberladung
+
+            Bruch b1 = new Bruch(1, 4);
+            Bruch b2 = new Bruch(1, 2);
+
+            Bruch ergebnis = b1 * b2;
+
+            Console.WriteLine($"Ergebnis ist: {ergebnis.Zähler}/{ergebnis.Nenner}");
 
             Console.WriteLine("---ENDE---");
             Console.ReadKey();
