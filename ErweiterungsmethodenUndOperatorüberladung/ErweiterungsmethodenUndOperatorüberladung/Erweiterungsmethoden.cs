9e1abf31b5a905bdd5e75ca040d2af3cc4fb8457
Michael Zöhling <michael.zoehling@outlook.com>
2019-04-10 16:11:17 +0200

    Erweiterungsmethoden

---
+++ ErweiterungsmethodenUndOperatorüberladung/ErweiterungsmethodenUndOperatorüberladung/Erweiterungsmethoden.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErweiterungsmethodenUndOperatorüberladung
+{
+    // Vorraussetzungen:
+    // 1) Statische Klasse
+    static class Erweiterungsmethoden
+    {
+        // 2) this - Schlüsselwort
+        public static string Umdrehen(this string input)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = input.Length - 1; i >= 0; i--) // forr + TAB + TAB
+            {
+                sb.Append(input[i]);
+            }
+            return sb.ToString();
+        }
+    }
+}
--- ErweiterungsmethodenUndOperatorüberladung/ErweiterungsmethodenUndOperatorüberladung/Program.cs
+++ ErweiterungsmethodenUndOperatorüberladung/ErweiterungsmethodenUndOperatorüberladung/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +9,11 @@ namespace ErweiterungsmethodenUndOperatorüberladung
     {
         static void Main(string[] args)
         {
+
+            // Erweiterungsmethoden:
+            string text = "Hallo Welt !";
+            Console.WriteLine(text.Umdrehen());
+            Console.WriteLine(Erweiterungsmethoden.Umdrehen(text));
 
             Console.WriteLine("---ENDE---");
             Console.ReadKey();
