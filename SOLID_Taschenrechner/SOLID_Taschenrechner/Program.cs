fc6ae1e4504de6276ebbb57fc2cd1640afb5683c
Michael Zöhling <michael.zoehling@outlook.com>
2019-04-11 09:22:27 +0200

    v1  - reiner Additions-Taschenrechner

--- SOLID_Taschenrechner/SOLID_Taschenrechner/Program.cs
+++ SOLID_Taschenrechner/SOLID_Taschenrechner/Program.cs
@@ -10,6 +10,20 @@ namespace SOLID_Taschenrechner
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Bitte geben Sie die Formel ein:");
+            string input = Console.ReadLine(); // "2 + 2"
+
+            string[] operanden = input.Split(); // Leerzeichen
+            int zahl1 = Convert.ToInt32(operanden[0]);
+            int zahl2 = Convert.ToInt32(operanden[2]);
+            string op = operanden[1];
+
+            int ergebnis = zahl1 + zahl2;
+
+            Console.WriteLine($"Das Ergebnis ist {ergebnis}");
+
+            Console.WriteLine("---ENDE---");
+            Console.ReadKey();
         }
     }
 }
