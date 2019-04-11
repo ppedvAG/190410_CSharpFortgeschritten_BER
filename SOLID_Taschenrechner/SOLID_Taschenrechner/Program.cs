227b067b7141811ad2e14831fdcf2d7f926b9c36
Michael Zöhling <michael.zoehling@outlook.com>
2019-04-11 09:34:12 +0200

    Logik in einzelne Klassen zerlegt

--- SOLID_Taschenrechner/SOLID_Taschenrechner/Program.cs
+++ SOLID_Taschenrechner/SOLID_Taschenrechner/Program.cs
@@ -8,21 +8,62 @@ namespace SOLID_Taschenrechner
 {
     class Program
     {
+        // Bootstrapping
         static void Main(string[] args)
+        {
+            new KonsolenUI().Start();
+        }
+    }
+
+    class Formel
+    {
+        public int Operand1 { get; set; }
+        public int Operand2 { get; set; }
+        public string Operator { get; set; }
+    }
+
+    class StringSplitParser
+    {
+        public Formel Parse(string input)
+        {
+            string[] operanden = input.Split(); // Leerzeichen
+            Formel result = new Formel();
+
+            result.Operand1 = Convert.ToInt32(operanden[0]);
+            result.Operand2 = Convert.ToInt32(operanden[2]);
+            result.Operator = operanden[1];
+
+            return result;
+        }
+    }
+
+    class SimplerRechner
+    {
+        public int Berechne(Formel formel)
+        {
+            if (formel.Operator == "+") // Entscheidungsfindung "welche rechenart"
+                return formel.Operand1 + formel.Operand2; // Berechnung
+            else if (formel.Operator == "-") // Entscheidungsfindung "welche rechenart"
+                return formel.Operand1 - formel.Operand2; // Berechnung 
+            // ...
+            else
+                throw new ArgumentException("Operator ist unbekannt");
+        }
+    }
+
+    class KonsolenUI
+    {
+        // Workflow
+        public void Start()
         {
             Console.WriteLine("Bitte geben Sie die Formel ein:");
             string input = Console.ReadLine(); // "2 + 2"
 
-            string[] operanden = input.Split(); // Leerzeichen
-            int zahl1 = Convert.ToInt32(operanden[0]);
-            int zahl2 = Convert.ToInt32(operanden[2]);
-            string op = operanden[1];
+            StringSplitParser parser = new StringSplitParser();
+            Formel formel = parser.Parse(input);
 
-            int ergebnis = 0;
-            if(op == "+")
-                ergebnis = zahl1 + zahl2;
-            else if(op == "-")
-                ergebnis = zahl1 - zahl2;
+            SimplerRechner rechner = new SimplerRechner();
+            int ergebnis = rechner.Berechne(formel);
 
             Console.WriteLine($"Das Ergebnis ist {ergebnis}");
 
