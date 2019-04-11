687f75092d5e43cfcabe93422ae7a657b53984b5
Michael Zöhling <michael.zoehling@outlook.com>
2019-04-11 11:52:55 +0200

    Init Attribute

---
+++ Attribut_Demo/Attribut_Demo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Attribut_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Console.WriteLine("---ENDE😘😘😘---");
+            Console.ReadKey();
+        }
+    }
+}
