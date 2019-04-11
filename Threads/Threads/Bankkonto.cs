daef7e16a41fce71f44ef72869687d3095bbd8be
Michael Zöhling <michael.zoehling@outlook.com>
2019-04-11 14:39:14 +0200

    Semaphore

---
+++ Threads/Threads/Bankkonto.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Threads
+{
+    class Bankkonto
+    {
+        public Bankkonto(decimal Kontostand)
+        {
+            this.Kontostand = Kontostand;
+            transaktion = 0;
+        }
+        private object lockObject = new object();
+        private static int transaktion;
+        public decimal Kontostand { get; set; }
+
+        public void Abheben(decimal Betrag)
+        {
+            lock (lockObject)
+            {
+                if (Kontostand >= Betrag)
+                {
+                    Console.WriteLine($"[{transaktion}]Kontostand vor dem Abheben: {Kontostand}€");
+                    Console.WriteLine($"[{transaktion}]Betrag zum Abheben: {Betrag}€");
+                    Kontostand -= Betrag;
+                    Console.WriteLine($"[{transaktion}]Kontostand nach dem Abheben: {Kontostand}€");
+                }
+                transaktion++;
+            }
+        }
+
+        public void Einzahlen(decimal Betrag)
+        {
+            Monitor.Enter(lockObject);
+
+            Console.WriteLine($"[{transaktion}]Kontostand vor dem Einzahlen: {Kontostand}€");
+            Console.WriteLine($"[{transaktion}]Betrag zum Einzahlen: {Betrag}€");
+            Kontostand += Betrag;
+            Console.WriteLine($"[{transaktion}]Kontostand nach dem Einzahlen: {Kontostand}€");
+            transaktion++;
+
+            Monitor.Exit(lockObject);
+        }
+    }
+}
--- Threads/Threads/Program.cs
+++ Threads/Threads/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Threads
@@ -10,8 +11,45 @@ namespace Threads
     {
         static void Main(string[] args)
         {
+            //konto = new Bankkonto(1000);
+
+            //for (int i = 0; i < 100; i++)
+            //    ThreadPool.QueueUserWorkItem(ZufälligesUpdate);
+
+            for (int i = 0; i < 100; i++)
+            {
+                ThreadPool.QueueUserWorkItem(MachWas);
+            }
+
+
             Console.WriteLine("---ENDE---");
             Console.ReadKey();
+        }
+        private static Semaphore semaphore = new Semaphore(3, 3);
+        private static object lockObject = new object();
+        private static int zähler = 0;
+        private static void MachWas(object state)
+        {
+            semaphore.WaitOne();
+            zähler++;
+            Console.WriteLine(zähler);
+            zähler--;
+            semaphore.Release();
+        }
+
+
+        private static Bankkonto konto;
+        private static Random r = new Random();
+
+        public static void ZufälligesUpdate(object state)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                if (r.Next(1, 100) < 50)
+                    konto.Einzahlen(r.Next(0, 1000));
+                else
+                    konto.Abheben(r.Next(0, 1000));
+            }
         }
     }
 }
