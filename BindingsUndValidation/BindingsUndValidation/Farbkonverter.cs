0fe873685a09c1505140e57cc53761aa3a679769
Michael Zöhling <michael.zoehling@outlook.com>
2019-04-12 15:05:28 +0200

    Unzählige Bindings

---
+++ BindingsUndValidation/BindingsUndValidation/Farbkonverter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace BindingsUndValidation
+{
+    class Farbkonverter : IValueConverter
+    {
+        // OneWay
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is null)
+                return new SolidColorBrush(Colors.Black);
+
+
+            string farbe = (string)value;
+
+            if (farbe == "Rot")
+                return Brushes.Red;
+            else if (farbe == "Grün")
+                return Brushes.Green;
+            else if (farbe == "Geil")
+                return new LinearGradientBrush
+                {
+                    GradientStops = new GradientStopCollection
+                    {
+                        new GradientStop{Offset = 0,Color=Colors.PapayaWhip},
+                        new GradientStop{Offset = 0.5,Color=Colors.Gray},
+                        new GradientStop{Offset = 1,Color=Colors.OldLace},
+                    }
+                };
+            else
+                return Brushes.Black;
+        }
+
+        // TwoWay
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BindingsUndValidation/BindingsUndValidation/RGBConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace BindingsUndValidation
+{
+    class RGBConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            byte R = System.Convert.ToByte(values[0]);
+            byte G = System.Convert.ToByte(values[1]);
+            byte B = System.Convert.ToByte(values[2]);
+
+            return new SolidColorBrush(Color.FromRgb(R, G, B));
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
