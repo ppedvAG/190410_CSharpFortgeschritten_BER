ac6ca8bf0b3778aad1e76df57d9fa289334d5f66
Michael Zöhling <michael.zoehling@outlook.com>
2019-04-12 09:50:43 +0200

    Basisversion ViewModel

---
+++ MVVM_Simpel/ViewModel/Helpers/RelayCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace ViewModel.Helpers
+{
+    internal class RelayCommand : ICommand
+    {
+        public RelayCommand(Action<object> executeAction, Func<object, bool> canExecuteFunc = null)
+        {
+            this.executeAction = executeAction ?? throw new ArgumentNullException(nameof(executeAction));
+            this.canExecuteFunc = canExecuteFunc ?? new Func<object, bool>(x => true);
+        }
+
+        private readonly Action<object> executeAction;
+        private readonly Func<object, bool> canExecuteFunc;
+
+        public event EventHandler CanExecuteChanged;
+        public bool CanExecute(object parameter) => canExecuteFunc.Invoke(parameter);
+
+        public void Execute(object parameter) => executeAction.Invoke(parameter);
+    }
+}
---
+++ MVVM_Simpel/ViewModel/MainViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Model.Contracts;
+using Model.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+using ViewModel.Helpers;
+
+namespace ViewModel
+{
+    public class MainViewModel
+    {
+        public MainViewModel()
+        {
+            // Kontrollfreak-Antipattern
+            service = new PersonenService();
+            GetPersonenCommand = new RelayCommand(GetPersonen);
+        }
+        private readonly PersonenService service;
+
+
+        public List<Person> Personenliste { get; set; }
+        public ICommand GetPersonenCommand { get; set; }
+        private void GetPersonen(object obj)
+        {
+            Personenliste = service.GetPersonen().ToList(); // Feature aus dem Model aufrufen
+        }
+    }
+}
